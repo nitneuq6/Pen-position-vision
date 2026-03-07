6699e4a794a2c7a3c0b345c3e22f6f9a754a6568
Quentin <quentinrol@hotmail.com>
2026-03-07 11:03:31 +0100

    Fixed cam color

--- PosVisionMain.py
+++ PosVisionMain.py
@@ -6,7 +6,7 @@ import cv2
 
 # Initialize camera
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (640, 480)})
+config = picam2.create_preview_configuration(main={"size": (640, 480), "format": "RGB888"})
 picam2.configure(config)
 picam2.start()
 
